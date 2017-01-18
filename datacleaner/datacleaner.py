5572555df1c51ed9627ab0e6ed5be64cad2486e7
Randy Olson <rso@randalolson.com>
2017-01-18 10:05:04 -0500

    Add note about special case with mode NA replacement

--- datacleaner/datacleaner.py
+++ datacleaner/datacleaner.py
@@ -77,7 +77,9 @@ def autoclean(input_dataframe, drop_nans=False, copy=False, encoder=None,
             input_dataframe[column].fillna(input_dataframe[column].median(), inplace=True)
         except TypeError:
             most_frequent = input_dataframe[column].mode()
-            if len(most_frequent)>0:
+            # If the mode can't be computed, use the nearest valid value
+            # See https://github.com/rhiever/datacleaner/issues/8
+            if len(most_frequent) > 0:
                 input_dataframe[column].fillna(input_dataframe[column].mode()[0], inplace=True)
             else:
                 input_dataframe[column].fillna(method='bfill', inplace=True)
